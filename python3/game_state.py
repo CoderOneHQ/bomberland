58f25e89952a7f678237038b441dbf2dc7caf8d4
Matthew <thegalah@gmail.com>
2021-04-02 16:51:39 -0700

    .

---
+++ python3/game_state.py
@@ -0,0 +1,10 @@
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Hello world!")
+        await websocket.recv()
+
+asyncio.get_event_loop().run_until_complete(hello())
