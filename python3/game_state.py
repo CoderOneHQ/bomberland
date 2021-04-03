727660ba432e65cd262538d5a425361027b47acb
Matthew <thegalah@gmail.com>
2021-04-02 17:03:23 -0700

    wip

--- python3/game_state.py
+++ python3/game_state.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def hello():
-    uri = "ws://localhost:8765"
+    uri = "ws://127.0.0.1:3000/?role=agent&agentId=agentId&name=defaultName"
     async with websockets.connect(uri) as websocket:
         await websocket.send("Hello world!")
         await websocket.recv()
