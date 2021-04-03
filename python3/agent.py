eb9f792f5ed4e06f9eb102fa10b14035ef1d5dc5
Matthew <thegalah@gmail.com>
2021-04-03 16:31:53 -0700

    wip

--- python3/agent.py
+++ python3/agent.py
@@ -1,16 +1,20 @@
-from game_state import GameState
+from game_state.game_state import GameState
 import asyncio
 
 uri = "ws://127.0.0.1:3000/?role=agent&agentId=agentId&name=defaultName"
 
 
+def generate_agent_action(tick_number, game_state):
+    print(tick_number)
+
+
 def main():
     client = GameState(uri)
+    client.set_game_tick_callback(generate_agent_action)
     loop = asyncio.get_event_loop()
     connection = loop.run_until_complete(client.connect())
     tasks = [
-        asyncio.ensure_future(client.heartbeat(connection)),
-        asyncio.ensure_future(client.receiveMessage(connection)),
+        asyncio.ensure_future(client._on_message(connection)),
     ]
 
     loop.run_until_complete(asyncio.wait(tasks))
---
+++ python3/game_state/game_state.py
@@ -0,0 +1,46 @@
+import asyncio
+import websockets
+import json
+
+
+class GameState:
+    def __init__(self, connection_string: str):
+        self._connection_string = connection_string
+        self._is_game_running = True
+        self._state = None
+        self._generate_agent_action_callback = None
+
+    def set_game_tick_callback(self, generate_agent_action_callback):
+        self._generate_agent_action_callback = generate_agent_action_callback
+
+    async def connect(self):
+        self.connection = await websockets.client.connect(self._connection_string)
+        if self.connection.open:
+            return self.connection
+
+    async def send_message(self, message):
+        await self.connection.send(message)
+
+    async def _on_message(self, connection):
+
+        while self._is_game_running is True:
+            try:
+                raw_data = await connection.recv()
+                data = json.loads(raw_data)
+                data_type = data.get("type")
+                payload = data.get("payload")
+                if data_type == "info":
+                    continue
+                if data_type == "game_state":
+                    self._on_game_state(payload)
+                else:
+                    print(f"unknown packet \"{data_type}\": {raw_data}")
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection with server closed')
+                break
+
+    def _on_game_state(self, game_state):
+        self._state = game_state
+
+    def _on_game_tick(self, game_tick):
+        print(game_tick)
