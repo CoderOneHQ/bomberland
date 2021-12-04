3edac50fb48b344bd9566b161236b540486c38a6
Matthew Duong <thegalah@gmail.com>
2021-12-04 13:44:20 -0800

    Update gym.py

--- python3/gym.py
+++ python3/gym.py
@@ -1,9 +1,18 @@
 from forward_model import ForwardModel
 
+class GymEnv:
+    def __init__(self, fwd_model: ForwardModel):
+        self._client_fwd = fwd_model
+
+    def reset(self):
+        print("resetting")
+
+    def step(self, action):
+        print("stepping")
 
 class Gym:
     def __init__(self, fwd_model_connection_string: str):
         self._client_fwd = ForwardModel(fwd_model_connection_string)
 
-    def make(self,name:str):
-        print(name)
+    def make(self,name:str) -> GymEnv:
+        return GymEnv(self._client_fwd)
