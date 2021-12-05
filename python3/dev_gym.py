4c0d653fe3d33ba9845f8b829b21a74a86c66e2f
Matthew <thegalah@gmail.com>
2021-12-05 07:41:11 -0800

    Update dev_gym.py

--- python3/dev_gym.py
+++ python3/dev_gym.py
@@ -1,16 +1,25 @@
+import asyncio
 from gym import Gym
 import os
 
 fwd_model_uri = os.environ.get(
     "FWD_MODEL_CONNECTION_STRING") or "ws://127.0.0.1:6969/?role=admin"
 
-gym = Gym(fwd_model_uri)
-env = gym.make("bomberland-open-ai-gym")
-observation = env.reset()
-for _ in range(1000):
-    actions = []
-    observation, reward, done, info = env.step(actions)
 
-    if done:
-        observation = env.reset()
-env.close()
+async def main():
+    gym = Gym(fwd_model_uri)
+    env = gym.make("bomberland-open-ai-gym")
+    observation = env.reset()
+    for _ in range(1000):
+        actions = []
+        observation, reward, done, info = env.step(actions)
+
+        if done:
+            observation = env.reset()
+    await env.close()
+
+loop = asyncio.get_event_loop()
+tasks = [
+    asyncio.ensure_future(main()),
+]
+loop.run_until_complete(asyncio.wait(tasks))
