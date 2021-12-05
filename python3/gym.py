32943e699f1c634698114ece7c3122212680fa19
Matthew <thegalah@gmail.com>
2021-12-05 09:59:58 -0800

    wip

--- python3/gym.py
+++ python3/gym.py
@@ -1,5 +1,5 @@
+import os
 import asyncio
-import json
 from typing import Dict
 from forward_model import ForwardModel
 
@@ -7,49 +7,44 @@ mock_6x6_state: Dict = {"units": [{"coordinates": [0, 1], "hp":3, "inventory":{"
     {"created": 0, "x": 0, "y": 3, "type": "m"}, {"created": 0, "x": 5, "y": 3, "type": "m"}, {"created": 0, "x": 4, "y": 3, "type": "m"}, {"created": 0, "x": 1, "y": 3, "type": "m"}, {"created": 0, "x": 3, "y": 5, "type": "m"}, {"created": 0, "x": 2, "y": 5, "type": "m"}, {"created": 0, "x": 5, "y": 4, "type": "m"}, {"created": 0, "x": 0, "y": 4, "type": "m"}, {"created": 0, "x": 1, "y": 1, "type": "w", "hp": 1}, {"created": 0, "x": 4, "y": 1, "type": "w", "hp": 1}, {"created": 0, "x": 3, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 2, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 5, "y": 5, "type": "w", "hp": 1}, {"created": 0, "x": 0, "y": 5, "type": "w", "hp": 1}, {"created": 0, "x": 4, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 1, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 5, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 0, "y": 0, "type": "w", "hp": 1}]}
 
 
-class GymEnv:
-    def __init__(self, connected_fwd_model: ForwardModel, initial_state: Dict):
-        self._fwd = connected_fwd_model
-        self._state = initial_state
-        self._sequenceId = 0
-
-    async def reset(self) -> None:
-        # print("resetting")
-        return
-
-    async def step(self, actions):
-        print("stepping with actions:{}".format(
-            json.dumps(actions, separators=(',', ':'))))
-        await self._fwd.send_next_state(
-            self._next_sequence_id(), self._state, actions)
-        return [1, 2, 3, 4]
-
-    async def close(self) -> None:
-        print("closing")
-        await self._fwd.close()
-
-    def _next_sequence_id(self) -> int:
-        self._sequenceId += 1
-        return self._sequenceId
+fwd_model_uri = os.environ.get(
+    "FWD_MODEL_CONNECTION_STRING") or "ws://127.0.0.1:6969/?role=admin"
 
 
-class Gym:
-    def __init__(self, fwd_model_connection_string: str):
-        self._fwd = ForwardModel(fwd_model_connection_string)
-        self._fwd.set_next_state_callback(self._on_next_game_state)
-        self._environments = {}
+class Gym():
+    def __init__(self):
+        self._client_fwd = ForwardModel(fwd_model_uri)
 
-    async def connect_forward_model(self):
-        client_fwd_connection = await self._fwd.connect()
+        self._client.set_game_tick_callback(self._on_game_tick)
+        self._client_fwd.set_next_state_callback(self._on_next_game_state)
+        self.connect()
+
+    def connect(self):
         loop = asyncio.get_event_loop()
-        loop.create_task(
-            self._fwd._handle_messages(client_fwd_connection))
 
-    def make(self, name: str, initial_state: Dict) -> GymEnv:
-        if self._environments.get(name) is not None:
-            raise Exception(
-                "Environment {} has already been instantiated".format(name))
-        return GymEnv(self._fwd, initial_state)
+        client_connection = loop.run_until_complete(self._client.connect())
+
+        client_fwd_connection = loop.run_until_complete(
+            self._client_fwd.connect())
+
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._client._handle_messages(client_connection))
+        loop.create_task(
+            self._client_fwd._handle_messages(client_fwd_connection))
+        loop.run_forever()
+
+    async def _send_eval_next_state(self):
+        actions = [
+            {
+                "action": {"move": "right", "type": "move"},
+                "agent_number": 0,
+            }, {
+                "action": {"move": "left", "type": "move"},
+                "agent_number": 1,
+            }
+        ]
+        await self._client_fwd.send_next_state(1, self._client._state, actions)
 
     async def _on_next_game_state(self, state):
-        print("mex")
+        print(state)
+        pass
