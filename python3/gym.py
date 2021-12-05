c84282f3c272fb239808339a232320fb5165859c
Matthew <thegalah@gmail.com>
2021-12-05 08:00:12 -0800

    Update gym.py

--- python3/gym.py
+++ python3/gym.py
@@ -1,9 +1,12 @@
-from forward_model import ForwardModel
 import json
+from typing import Dict
+mock_6x6_state = {"units": [{"coordinates": [0, 1], "hp":3, "inventory":{"bombs": 3}, "blast_diameter": 3, "unit_id": "c", "agent_id": "a", "invulnerability": 0}, {"coordinates": [5, 1], "hp":3, "inventory":{"bombs": 3}, "blast_diameter": 3, "unit_id": "d", "agent_id": "b", "invulnerability": 0}, {"coordinates": [3, 3], "hp":3, "inventory":{"bombs": 3}, "blast_diameter": 3, "unit_id": "e", "agent_id": "a", "invulnerability": 0}, {"coordinates": [2, 3], "hp":3, "inventory":{"bombs": 3}, "blast_diameter": 3, "unit_id": "f", "agent_id": "b", "invulnerability": 0}, {"coordinates": [2, 4], "hp":3, "inventory":{"bombs": 3}, "blast_diameter": 3, "unit_id": "g", "agent_id": "a", "invulnerability": 0}, {"coordinates": [3, 4], "hp":3, "inventory":{"bombs": 3}, "blast_diameter": 3, "unit_id": "h", "agent_id": "b", "invulnerability": 0}], "entities": [
+    {"created": 0, "x": 0, "y": 3, "type": "m"}, {"created": 0, "x": 5, "y": 3, "type": "m"}, {"created": 0, "x": 4, "y": 3, "type": "m"}, {"created": 0, "x": 1, "y": 3, "type": "m"}, {"created": 0, "x": 3, "y": 5, "type": "m"}, {"created": 0, "x": 2, "y": 5, "type": "m"}, {"created": 0, "x": 5, "y": 4, "type": "m"}, {"created": 0, "x": 0, "y": 4, "type": "m"}, {"created": 0, "x": 1, "y": 1, "type": "w", "hp": 1}, {"created": 0, "x": 4, "y": 1, "type": "w", "hp": 1}, {"created": 0, "x": 3, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 2, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 5, "y": 5, "type": "w", "hp": 1}, {"created": 0, "x": 0, "y": 5, "type": "w", "hp": 1}, {"created": 0, "x": 4, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 1, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 5, "y": 0, "type": "w", "hp": 1}, {"created": 0, "x": 0, "y": 0, "type": "w", "hp": 1}]}from forward_model import ForwardModel
 
 
 class GymEnv:
-    def __init__(self, fwd_model: ForwardModel):
+    def __init__(self, fwd_model: ForwardModel, initial_state: Dict):
+        self._state = initial_state
         self._fwd = fwd_model
         self._sequenceId = 0
 
@@ -24,6 +27,11 @@ class GymEnv:
 class Gym:
     def __init__(self, fwd_model_connection_string: str):
         self._client_fwd = ForwardModel(fwd_model_connection_string)
+        self._client_fwd.connect()
+        self._environments = {}
 
-    def make(self, name: str) -> GymEnv:
-        return GymEnv(self._client_fwd)
+    def make(self, name: str, initial_state: Dict) -> GymEnv:
+        if self._environments.get(name) is not None:
+            raise Exception(
+                "Environment {} has already been instantiated".format(name))
+        return GymEnv(self._client_fwd, initial_state)
