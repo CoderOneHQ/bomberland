e08d0ee9e92abf277544b9958295507d277d4d72
Matthew <thegalah@gmail.com>
2021-12-05 07:38:17 -0800

    fwd model close

--- python3/dev_gym.py
+++ python3/dev_gym.py
@@ -5,7 +5,7 @@ fwd_model_uri = os.environ.get(
     "FWD_MODEL_CONNECTION_STRING") or "ws://127.0.0.1:6969/?role=admin"
 
 gym = Gym(fwd_model_uri)
-env = gym.make("CartPole-v1")
+env = gym.make("bomberland-open-ai-gym")
 observation = env.reset()
 for _ in range(1000):
     actions = []
--- python3/forward_model.py
+++ python3/forward_model.py
@@ -1,12 +1,14 @@
 import websockets
 import json
-import copy
 
 
 class ForwardModel:
     def __init__(self, connection_string: str):
         self._connection_string = connection_string
         self._next_state_callback = None
+
+    async def close(self):
+        await self.connection.close()
 
     def set_next_state_callback(self, next_state_callback):
         self._next_state_callback = next_state_callback
