f56719d9041e457664684c6fb2642607f922139f
Matthew <thegalah@gmail.com>
2022-04-17 15:37:25 -0700

    init

---
+++ engine/bomberland-ui/gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Bomberland Ui`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
+export default config;
