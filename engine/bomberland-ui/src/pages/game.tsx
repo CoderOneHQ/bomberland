d6cb856346695fbcd9dee9d718e64b5316c476ef
Matthew <thegalah@gmail.com>
2022-04-17 18:45:58 -0700

    Update game.tsx

--- engine/bomberland-ui/src/pages/game.tsx
+++ engine/bomberland-ui/src/pages/game.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Bomberman from "../components/PageSwitch/pages/Game/Bomberman.page";
+import Bomberman from "../components/Game/Bomberman.page";
 import { SSRGuard } from "../components/SSRGuard/SSRGuard";
 const Page: React.FC = () => {
     return (
