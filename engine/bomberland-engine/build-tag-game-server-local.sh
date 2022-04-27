31c19a8720d3779d0b70cabec0b7fcd0e5dcce84
Matthew <thegalah@gmail.com>
2022-04-26 21:51:48 -0700

    cleanup

--- engine/bomberland-engine/build-tag-game-server-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pushd ../
-docker build -f ./game-server/Dockerfile . --target build -t gs-build-local --build-arg ENVIRONMENT=dev --build-arg BUILD=0
-CID=$(docker create gs-build)
-docker cp ${CID}:/app/game-server/Program.exe Program.exe
-docker cp ${CID}:/app/game-server/Program Program
-docker rm ${CID}
-docker build -f ./game-server/Dockerfile . --build-arg ENVIRONMENT=dev -t gs-local --build-arg BUILD=0
-rm Program.exe Program
