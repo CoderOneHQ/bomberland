15351d814a039487abef172ce943949da251d3fb
Matthew <thegalah@gmail.com>
2022-05-02 02:35:35 -0700

    Update gatsby-config.ts

--- engine/bomberland-ui/gatsby-config.ts
+++ engine/bomberland-ui/gatsby-config.ts
@@ -4,7 +4,16 @@ const config: GatsbyConfig = {
     siteMetadata: {
         title: `Bomberland Ui`,
     },
-    plugins: ["gatsby-plugin-styled-components"],
+    plugins: [
+        "gatsby-plugin-styled-components",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown-pages`,
+                path: `${__dirname}/src/source-filesystem/markdown-pages`,
+            },
+        },
+    ],
 };
 
 export default config;
