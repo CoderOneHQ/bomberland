94b1c5d510deaf40844256d9727918dcdb3b73d6
Matthew <thegalah@gmail.com>
2022-05-02 23:45:00 -0700

    wip

--- engine/bomberland-ui/src/components/DocumentationContent/DocumentationContent.tsx
+++ engine/bomberland-ui/src/components/DocumentationContent/DocumentationContent.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useTranslation, Trans } from "react-i18next";
 import { Grid, Root, Headline, InformationHeader } from "./DocumentationContent.styles";
 import { RouterLink } from "../RouterLink/RouterLink";
-import { CoderOneRoute } from "../Pages/CoderOneRoute";
+import { CoderOneRoute } from "../../utilities/CoderOneRoute";
+import { TelemetryLink } from "../TelemetryLink/TelemetryLink";
 
 interface IProps {
     readonly posts: JSX.Element;
@@ -16,7 +17,8 @@ export const DocumentationContent: React.FC<IProps> = ({ posts }) => {
             <InformationHeader>
                 <Headline>{t("documentation")}</Headline>
                 <Trans key="documentationDescription">
-                    Resources for interacting with the <RouterLink href={CoderOneRoute.Bomberland}>Bomberland</RouterLink> environment.
+                    Resources for interacting with the <TelemetryLink href={CoderOneRoute.Bomberland}>Bomberland</TelemetryLink>{" "}
+                    environment.
                 </Trans>
             </InformationHeader>
             <Grid>{posts}</Grid>
--- engine/bomberland-ui/src/utilities/CoderOneRoute.ts
+++ engine/bomberland-ui/src/utilities/CoderOneRoute.ts
@@ -1,4 +1,5 @@
 export abstract class CoderOneRoute {
+    public static readonly Bomberland = "https://www.gocoder.one/bomberland";
     public static readonly DocumentationIndex = "/docs";
     public static readonly Game = "/game";
     public static readonly GameInstanceRoute = (connectionString: string): string => {
