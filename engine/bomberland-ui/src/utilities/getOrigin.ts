360210d0e9bb2173abb2d0a05a9d6a801a4e3985
Matthew <thegalah@gmail.com>
2022-07-13 00:47:24 -0700

    Update getOrigin.ts

--- engine/bomberland-ui/src/utilities/getOrigin.ts
+++ engine/bomberland-ui/src/utilities/getOrigin.ts
@@ -1,6 +1,6 @@
 export const getOrigin = () => {
     if (typeof window !== "undefined") {
-        return `${window.location.protocol}//${window.location.hostname}`;
+        return `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
     } else {
         return ``;
     }
