0a801c65f43b6a38c8d0df9e433cb76a13002d2b
Matthew <thegalah@gmail.com>
2022-07-17 21:29:12 -0700

    Update routers.ts

--- engine/bomberland-engine/src/Api/routers.ts
+++ engine/bomberland-engine/src/Api/routers.ts
@@ -1,5 +1,6 @@
+import { indexRouter } from "./index.router";
 import Router from "koa-router";
 
 export interface IServices {}
 
-export const routers: Array<(services: IServices) => Router> = [];
+export const routers: Array<(services: IServices) => Router> = [indexRouter];
